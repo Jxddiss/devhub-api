3bed4f954a2c69f917f12ea8e7206da53b75d775
Ali <alibenkarrouch@gmail.com>
2025-04-03 23:56:12 -0400

    Ajout de l'attribut parentComment au retour

--- src/services/commentService.ts
+++ src/services/commentService.ts
@@ -9,19 +9,19 @@ export const createComment = async (data: Partial<Comment>) => {
 }
 
 export const getAllCommentsByProjet = async (projetId: number) => {
-    return commentRepository.find({ where: { projet: { id: projetId } }, relations: ["author", "projet"] });
+    return commentRepository.find({ where: { projet: { id: projetId } }, relations: ["author", "projet","parentComment"] });
 }
 
 export const getCommentById = async (id: number) => {
-    return commentRepository.findOne({ where: { id }, relations: ["author", "projet"] });
+    return commentRepository.findOne({ where: { id }, relations: ["author", "projet","parentComment"] });
 }
 
 export const getAllCommentsByUserId = async (userId: number) => {
-    return commentRepository.find({ where: { author: { id: userId } }, relations: ["author", "projet"] });
+    return commentRepository.find({ where: { author: { id: userId } }, relations: ["author", "projet","parentComment"] });
 }
 
 export const getAllRepliesByCommentId = async (commentId: number) => {
-    return commentRepository.find({ where: { parentComment: { id: commentId } }, relations: ["author", "projet"] });
+    return commentRepository.find({ where: { parentComment: { id: commentId } }, relations: ["author", "projet","parentComment"] });
 }
 
 export const deleteComment = async (id: number) => {
